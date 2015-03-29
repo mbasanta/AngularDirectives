635a6eb19287fd9096c33e6d2f4fdf3e39eb15e8
Matthew Basanta <matthew.basanta@gmail.com>
2015-03-28 22:23:48 -0400

    Add spacebar support decorator directive

---
+++ module-3/spacebarSupport.js
@@ -0,0 +1,27 @@
+(function(angular) {
+
+angular.module('app').directive('spacebarSupport', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, el, attrs) {
+      $('body').on('keypress', function(evt){
+
+        var vidEl = el[0];
+
+        if(evt.keyCode === 32) {
+
+          if (vidEl.paused) {
+            vidEl.play();
+          } else {
+            vidEl.pause();
+          }
+
+        }
+
+      });
+
+    }
+  };
+});
+
+}(angular));
