32278907da5b33282eaa6d2b0e39333a51482e71
Matthew Basanta <matthew.basanta@gmail.com>
2015-03-31 20:26:19 -0400

    Set up boilerplate with ng-repeat

--- module-5.4/MainCtrl.js
+++ module-5.4/MainCtrl.js
@@ -3,6 +3,24 @@
 
 angular.module('app').controller('MainCtrl', function($scope) {
 
+  $scope.bountyHunters = [{
+    name: 'Boba Fett'
+  },{
+    name: 'Dengar'
+  },{
+    name: 'Bossk'
+  },{
+    name: 'Cad Bane'
+  }];
+
+  $scope.add = function() {
+    $scope.bountyHunters.push({name: '4LOM'});
+  };
+
+  $scope.remove = function() {
+    $scope.bountyHunters.length--;
+  };
+
 });
 
 }(angular));
