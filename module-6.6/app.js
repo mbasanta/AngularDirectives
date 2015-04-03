581cf82601c49a2dcf23595e952514f799f5af4f
Matthew Basanta <matthew.basanta@gmail.com>
2015-04-03 09:16:26 -0400

    Set up Panel directives

---
+++ module-6.6/app.js
@@ -0,0 +1,47 @@
+angular.module('plunker', []);
+
+angular.module('plunker').controller('MainCtrl', function($scope) {
+
+});
+
+angular.module('plunker').directive('swTabstrip', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {},
+    controller: function($scope) {
+      $scope.panes = [];
+      $scope.select = function(pane) {
+        pane.selected = true;
+        $scope.panes.forEach(function(curPane) {
+          if (curPane !== pane) {
+            curPane.selected = false;
+          }
+        });
+      };
+
+      this.addPane = function(pane) {
+        $scope.panes.push(pane);
+        if ($scope.panes.length === 1) {
+          pane.selected = true;
+        }
+      };
+    },
+    templateUrl: 'swTabstrip.html'
+  };
+});
+
+angular.module('plunker').directive('swPane', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      title: '@'
+    },
+    require: '^swTabstrip',
+    link: function(scope, el, attrs, tabstripCtrl) {
+      tabstripCtrl.addPane(scope);
+    },
+    templateUrl: 'swPane.html'
+  };
+});
